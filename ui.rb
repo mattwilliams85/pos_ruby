028ad4c397323fe74361500b4a344ec4dc553443
Matthew <mattjunk85@yahoo.com>
2014-08-24 13:39:42 -0700

    add add_inventory and update_item methods

--- ui.rb
+++ ui.rb
@@ -31,6 +31,21 @@ def wait
 	sleep 0.85
 end 
 
+def long_wait
+	sleep 1.5
+end 
+
+# def singular?
+#   @name = @name + "s have" 
+# 	@name = @name + " has" if @quantity.to_i == 1
+# end
+
+def list_items
+	puts "-------------------"
+	Item.all.sort.each {|i| puts "##{i.id} - #{i.name}"}
+	puts "-------------------"
+end
+
 def main_menu
 	until @input == '3'
 		header
@@ -40,7 +55,8 @@ def main_menu
 		@input = gets.chomp
 		case @input
 		when '1'
-			login_menu
+			## RETURN TO login_menu after development
+			clerk_menu
 		when '2'
 			new_user
 		when '3'
@@ -79,6 +95,7 @@ def login_menu
 			password = gets.chomp
 			if clerk.password == password
 				puts "Welcome #{name}!"
+				wait
 				clerk_menu
 			else 
 				puts "Password incorrect"
@@ -93,7 +110,50 @@ def login_menu
 end
 
 def clerk_menu
+		until @input == '4'
+		header
+		puts "1 > New Transaction"
+		puts "2 > Add Inventory"
+		puts "3 > Update Inventory"
+		puts "4 > Return to Main Menu"
+		@input = gets.chomp
+		case @input
+		when '1'
+			new_transaction
+		when '2'
+			add_inventory
+		when '3'
+			update_inventory
+		when '4'
+			main_menu
+		else 
+			invalid 
+			main_menu
+		end
+	end
+end
 
+def add_inventory
+	puts "Enter the name of your new item (singular):"
+	name = gets.chomp.downcase.capitalize
+	puts "Enter the price of item (0.00):"
+	price = gets.chomp
+	Item.create({:name => name, :price => price})
+	puts "'#{name}' for $#{price} has been added to the inventory!"
+	long_wait
+end
+
+def update_inventory
+	list_items
+	puts "Enter the # of the item to update:"
+	id = gets.chomp.to_i
+	puts "Enter the new name of the item:"
+	new_name = gets.chomp.downcase.capitalize
+	puts "Enter the new price for the item (0.00):"
+  new_price = gets.chomp
+  Item.find(id).update(:name => new_name, :price => new_price)
+  puts "Item ##{id} will now be #{new_name} for $#{new_price}!"
+  long_wait
 end
 
 main_menu
